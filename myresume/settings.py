95325500dd7a4d2b340eb8453dba0a17020c9658
steve-ongera <gadafisteve001@gmail.com>
2024-10-22 19:10:24 +0300

    new static changes

--- myresume/settings.py
+++ myresume/settings.py
@@ -26,6 +26,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+   
     'steve',
 ]
 
@@ -110,6 +111,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS=[
     os.path.join(BASE_DIR , 'static'),
 ]
