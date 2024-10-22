5f8d88b9d3bd517d98c93af6baa74fcc513d8cda
steve-ongera <gadafisteve001@gmail.com>
2024-10-23 01:35:27 +0300

    eerroe

--- myresume/settings.py
+++ myresume/settings.py
@@ -116,7 +116,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles', 'static')
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
