ba674b666f3e4604e985ae5fd16d2d6ba203b9df
steve-ongera <gadafisteve001@gmail.com>
2024-10-29 18:55:27 +0300

    new changes

--- myresume/settings.py
+++ myresume/settings.py
@@ -114,7 +114,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 
 STATIC_URL = "static/"
